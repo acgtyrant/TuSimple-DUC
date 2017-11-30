6c7fe920342bb7ee3f1dafda75bfec416c3ee98a
acgtyrant <acgtyrant@gmail.com>
2017-11-30 19:48:05 +0800

    get_cityscapes_list.py: complete

--- data_prep/get_cityscapes_list.py
+++ data_prep/get_cityscapes_list.py
@@ -1,28 +1,45 @@
+#!/usr/bin/env python
 import os
 import glob
 
 
-def get_cityscapes_list_augmented(root, image_path, label_path, lst_path, is_fine=True, sample_rate=1):
+def build_list(
+        root, image_path, label_path, lst_path,
+        is_fine=True, sample_rate=1, is_crop=True):
     index = 0
-    train_lst = []
+    lst = []
     label_prefix = 'gtFine_labelIds' if is_fine else 'gtCoarse_labelIds'
 
-    # images
     all_images = glob.glob(os.path.join(root, image_path, '*/*.png'))
     all_images.sort()
     for p in all_images:
-        l = p.replace(image_path, label_path).replace('leftImg8bit', label_prefix)
-        if os.path.isfile(l):
-            index += 1
-            if index % 100 == 0:
-                print "%d out of %d done." % (index, len(all_images))
-            if index % sample_rate != 0:
-                continue
-            for i in range(1, 8):
-                train_lst.append([str(index), p, l, "512", str(256 * i)])
-        else:
-            print "dismiss %s" % (p)
+        l = p.replace(
+                image_path, label_path).replace('leftImg8bit', label_prefix)
+        index += 1
+        if index % sample_rate == 0:
+            if is_crop:
+                for i in range(1, 8):
+                    lst.append([str(index), p, l, "512", str(256 * i)])
+            else:
+                lst.append([str(index), p, l])
 
     train_out = open(lst_path, "w")
-    for line in train_lst:
+    for line in lst:
         print >> train_out, '\t'.join(line)
+
+
+def main():
+    root = '/home/acgtyrant/BigDatas/Cityscapes'
+    train_image_path = 'leftImg8bit/train'
+    train_label_path = 'gtFine/train'
+    train_lst_path = 'train_bigger_patch.lst'
+    build_list(
+            root, train_image_path, train_label_path, train_lst_path)
+    val_image_path = 'leftImg8bit/val'
+    val_label_path = 'gtFine/val'
+    val_lst_path = 'val_bigger_patch.lst'
+    build_list(
+            root, val_image_path, val_label_path, val_lst_path, is_crop=False)
+
+if __name__ == '__main__':
+    main()
